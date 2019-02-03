02fcaacce36498e393352fe1fcc1efb1f7fbcd65
user <user@DESKTOP-HTL37BT>
2019-02-03 22:08:56 +0530

    intial commit

---
+++ src/main/java/com/boot/git/app/SpringBootGitApplication.java
@@ -0,0 +1,20 @@
+package com.boot.git.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class SpringBootGitApplication {
+	public static void main(String[] args) {
+	SpringApplication.run(SpringBootGitApplication.class, args);
+	}
+}
---
+++ src/main/java/com/boot/git/app/controller/GitController.java
@@ -0,0 +1,28 @@
+package com.boot.git.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// http://localhost:8090/gitController/sample
+
+@Controller
+@RequestMapping(value = "/gitController")
+public class GitController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GitController.class.getSimpleName());
+
+	@RequestMapping(value = "/sample", produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> sampleController() {
+		LOGGER.info("I am in sampleController dude......");
+		return new ResponseEntity<String>("Sucessfully done", HttpStatus.OK);
+	}
+
+}
